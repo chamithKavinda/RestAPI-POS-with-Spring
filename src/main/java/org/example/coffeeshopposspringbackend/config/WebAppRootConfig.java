5b712079210e82e78c89288ed72627cc76cba64a
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-04 09:44:12 +0530

    Add configs for DriverManagerDatasource

--- src/main/java/org/example/coffeeshopposspringbackend/config/WebAppRootConfig.java
+++ src/main/java/org/example/coffeeshopposspringbackend/config/WebAppRootConfig.java
@@ -1,9 +1,50 @@
 package org.example.coffeeshopposspringbackend.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("org.example.coffeeshopposspringbackend")
+@EnableJpaRepositories(basePackages = "org.example.coffeeshopposspringbackend")
+@EnableTransactionManagement
 public class WebAppRootConfig {
+    @Bean
+    public DataSource dataSource() {
+        var dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/coffeeshopposspring?createDatabaseIfNotExist=true");
+        dmds.setUsername("root");
+        dmds.setPassword("CK13");
+        return dmds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("org.example.coffeeshopposspringbackend.entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 }
