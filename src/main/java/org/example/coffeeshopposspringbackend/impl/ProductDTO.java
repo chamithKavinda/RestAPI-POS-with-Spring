b9998af1b9852200afc309d65838372978f67806
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-08 21:58:18 +0530

    ProductDao, ProductDTO, ProductService, ProductServiceIMPL added

---
+++ src/main/java/org/example/coffeeshopposspringbackend/impl/ProductDTO.java
@@ -0,0 +1,19 @@
+package org.example.coffeeshopposspringbackend.impl;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.coffeeshopposspringbackend.dto.SuperDTO;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductDTO implements SuperDTO {
+    private String pro_id;
+    private String pro_name;
+    private String pro_img;
+    private String price;
+    private String category;
+    private String quantity;
+}
