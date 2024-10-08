df226085acdebbc3fb78e23b88a14ebc9cd77b11
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-08 17:51:58 +0530

    CustomerResponse , CustomerErrorResponse added

---
+++ src/main/java/org/example/coffeeshopposspringbackend/customeobj/CustomerErrorResponse.java
@@ -0,0 +1,15 @@
+package org.example.coffeeshopposspringbackend.customeobj;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerErrorResponse implements CustomerResponse, Serializable {
+    private int errorCode;
+    private String errorMessage;
+}
