3ed2248fc1411bfab399f187ffb422a18aeb3016
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-09 12:00:40 +0530

    ProductNotFoundException implemented

---
+++ src/main/java/org/example/coffeeshopposspringbackend/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.coffeeshopposspringbackend.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(){}
+    public ProductNotFoundException(String message){}
+    public ProductNotFoundException(String message, Throwable cause){}
+}
