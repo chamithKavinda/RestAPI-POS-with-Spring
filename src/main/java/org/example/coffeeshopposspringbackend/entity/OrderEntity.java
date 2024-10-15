7ef575ff5b2c68b667baaa973ee5b758fbcad85e
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-15 09:16:04 +0530

    OrderEntity implement

---
+++ src/main/java/org/example/coffeeshopposspringbackend/entity/OrderEntity.java
@@ -0,0 +1,24 @@
+package org.example.coffeeshopposspringbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "order")
+@Entity
+public class OrderEntity implements SuperEntity{
+    @Id
+    private String orderId;
+    @ManyToOne
+    @JoinColumn(name = "custContact", referencedColumnName = "custContact")
+    private CustomerEntity customer;
+    private LocalDateTime orderDateTime;
+
+
+}
