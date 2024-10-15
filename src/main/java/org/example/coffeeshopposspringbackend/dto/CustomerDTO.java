ba25af2744435cf904d370c7a84de5498b713f57
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-15 16:53:46 +0530

    spring validation apply for customer

--- src/main/java/org/example/coffeeshopposspringbackend/dto/CustomerDTO.java
+++ src/main/java/org/example/coffeeshopposspringbackend/dto/CustomerDTO.java
@@ -1,5 +1,8 @@
 package org.example.coffeeshopposspringbackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,13 @@ import org.example.coffeeshopposspringbackend.dto.SuperDTO;
 @NoArgsConstructor
 @Data
 public class CustomerDTO implements SuperDTO , CustomerResponse {
+    @NotBlank(message = "Customer Contact cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Customer Contact must be a 10-digit number")
     private String custContact;
+    @NotBlank(message = "Customer Address cannot be empty")
     private String custAddress;
+    @Pattern(regexp = "^C\\d{3}$", message = "Customer Id must be in the format C001")
     private String custId;
+    @NotBlank(message = "Customer Name cannot be empty")
     private String custName;
 }
