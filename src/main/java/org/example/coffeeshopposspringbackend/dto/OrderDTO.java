1b7fc243b74ee5a6aaca6b63e154d1c777698e53
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-15 16:54:34 +0530

    spring validation apply for Order

--- src/main/java/org/example/coffeeshopposspringbackend/dto/OrderDTO.java
+++ src/main/java/org/example/coffeeshopposspringbackend/dto/OrderDTO.java
@@ -1,8 +1,12 @@
 package org.example.coffeeshopposspringbackend.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.coffeeshopposspringbackend.customeobj.OrderResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,9 +14,12 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class OrderDTO implements SuperDTO {
+public class OrderDTO implements SuperDTO , OrderResponse {
     private String orderId;
+    @NotBlank(message = "Customer Contact cannot be blank")
     private String custContact;
     private LocalDateTime orderDateTime;
+    @NotEmpty(message = "Order must contain at least one order detail")
+    @Valid
     private List<OrderDetailDTO> orderDetails;
 }
